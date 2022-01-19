714395b3373cf0a47a94795725298a832ee11f5e
Fany Alvarez <alvarezpestefania@outlook.com>
2022-01-19 15:34:30 -0600

    add estilos calendario

--- pages/principal/Buscador.jsx
+++ pages/principal/Buscador.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Layout from '../components/Layout'
-import Search from '../components/Search/Search'
+import Layout from '../../components/Layout'
+import Search from '../../components/Search/Search'
 
 function Buscador () {
   return (
