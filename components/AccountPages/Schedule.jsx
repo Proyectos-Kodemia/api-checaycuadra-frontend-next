e8a28a98236b1cd267ce3ed1d8d9e1619cfcddf5
Fany Alvarez <alvarezpestefania@outlook.com>
2022-01-27 00:47:07 -0600

    add modificaciones Schedule

--- components/AccountPages/Schedule.jsx
+++ components/AccountPages/Schedule.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { Button, Stack, Typography, Box } from '@mui/material'
 import moment from 'moment'
-import 'moment/locale/es'
+import 'moment/locale/es-mx'
 import isWeekend from 'date-fns/isWeekend'
 import TextField from '@mui/material/TextField'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import StaticDatePicker from '@mui/lab/StaticDatePicker'
-import TimePicker from '@mui/lab/TimePicker'
+import StaticTimePicker from '@mui/lab/StaticTimePicker'
 import Collapse from '@mui/material/Collapse'
 import IconButton from '@mui/material/IconButton'
 import List from '@mui/material/List'
@@ -67,7 +67,7 @@ function Schedule () {
   // const add = Horas.push(selectedStar)
   // console.log(add)
   const hourSelected = (moment(selectedStar).hour())
-  const hourFormat = moment().day(hourSelected).format('LT')
+  const hourFormat = moment().hour(hourSelected).format('H')
   console.log(hourFormat)
 
   const daySelected = (moment(selectedDate).date())
@@ -75,25 +75,9 @@ function Schedule () {
   const nameWeek = moment().day(numberWeek).format('dddd')
   console.log(daySelected, nameWeek)
 
-  const nextNumber1 = moment().date(daySelected).add(1, 'd').date()
-  const nextName1 = moment().date(daySelected).add(1, 'd').format('dddd')
-  console.log(nextNumber1, nextName1)
-
-  const nextNumber2 = moment().date(daySelected).add(2, 'd').date()
-  const nextName2 = moment().date(daySelected).add(2, 'd').format('dddd')
-  console.log(nextNumber2, nextName2)
-
-  const nextNumber3 = moment().date(daySelected).add(3, 'd').date()
-  const nextName3 = moment().date(daySelected).add(3, 'd').format('dddd')
-  console.log(nextNumber3, nextName3)
-
-  const nextNumber4 = moment().date(daySelected).add(4, 'd').date()
-  const nextName4 = moment().date(daySelected).add(4, 'd').format('dddd')
-  console.log(nextNumber4, nextName4)
-
   const numberMonth = (moment(selectedDate).month())
   const nameMonth = moment().month(numberMonth).format('MMMM')
-  // console.log(nameMonth)
+  console.log(nameMonth)
 
   const [InBasket, setInBasket] = React.useState(Horas.slice())
 
@@ -118,7 +102,7 @@ function Schedule () {
         <Box sx={{ p: 2, width: 450, fontSize: '3 rem', fontWeight: '700' }}>
           1. Selecciona el dia<br />
         </Box>
-        <Box sx={{ textAling: 'center', p: 2 }}>
+        <Box sx={{ textAling: 'center', mx: 'auto', mb: 5 }}>
           <LocalizationProvider dateAdapter={AdapterDateFns}>
             <StaticDatePicker
               format='MM-DD-YYYY'
@@ -135,16 +119,23 @@ function Schedule () {
           </LocalizationProvider>
         </Box>
         <br />
-        <Box sx={{ p: 2, width: 450, fontSize: '3 rem', fontWeight: '700' }}>
+        <Box sx={{ p: '2 2 0 2', width: 450, fontSize: '3 rem', fontWeight: '700' }}>
           2. Selecciona mas horas<br /><br />
+          <Box sx={{ fontSize: '0 rem', fontWeight: '200' }}>
+            Recuerda solo se utilizan horas completas<br /><br />
+          </Box>
         </Box>
 
         <Box sx={{ textAling: 'center', mx: 'auto', mb: 5 }}>
           <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <TimePicker
+            <StaticTimePicker
+              displayStaticWrapperAs='mobile'
               value={selectedStar}
               onChange={changeStar}
               renderInput={(params) => <TextField {...params} />}
+              views='hours'
+              am PM={false}
+
             />
           </LocalizationProvider>
         </Box>
